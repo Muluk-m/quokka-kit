06bbfc1738e411050fbd1022fb2e646d573f523d
maqiqian <1363964820@qq.com>
2024-05-18 18:38:57 +0800

    fix: package name don't need sort

--- packages/create/utils/sortDependencies.ts
+++ packages/create/utils/sortDependencies.ts
@@ -1,7 +1,7 @@
 export default function sortDependencies(packageJson) {
   const sorted = {}
 
-  const depTypes = ['name', 'dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']
+  const depTypes = ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']
 
   for (const depType of depTypes) {
     if (packageJson[depType]) {
