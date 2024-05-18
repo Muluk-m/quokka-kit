b073e39ae08c99d63e7b15bc5376e6831b1dd9e8
maqiqian <1363964820@qq.com>
2024-05-18 18:38:29 +0800

    chore: update docker rc map

--- bin/prepublishOnly.sh
+++ bin/prepublishOnly.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --privileged -v $(pwd):/app -w /app node:16-slim bash -c "bash ./bin/preinstall.sh"
+docker run --rm --privileged -v $(pwd):/app -v ~/.npmrc:~/.npmrc -w /app node:16-slim bash -c "bash ./bin/preinstall.sh"
